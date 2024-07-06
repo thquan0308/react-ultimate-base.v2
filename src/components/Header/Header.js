8a2ce330874ff9a19aa6fb057d42b1e7ba620e6a
thquan0308 <tranhoangquanthq@gmail.com>
2024-07-06 18:02:14 +0700

    3 update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,10 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand> */}
+                <NavLink to='/' className='navbar-brand'>Hỏi Dân IT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -23,11 +24,11 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >Log in</NavDropdown.Item>
+                            <NavDropdown.Item >
                                 Log out
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
 
                         </NavDropdown>
                     </Nav>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,16 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
+
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            <video autoPlay muted loop >
+                <source
+                    src={videoHomepage}
+                    type="video/mp4"
+                />
+            </video>
+        </div>
+    )
+}
+
+export default HomePage
