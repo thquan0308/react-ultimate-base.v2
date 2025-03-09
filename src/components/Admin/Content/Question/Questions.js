552eeb4d460ca7f7dfcd4cd29fff771cd22f99fb
thquan0308 <tranhoangquanthq@gmail.com>
2025-03-09 22:40:52 +0700

    api add new

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Select from "react-select"
 import './Questions.scss'
 import { BsFillPatchPlusFill, BsFillPatchMinusFill } from 'react-icons/bs'
@@ -7,15 +7,15 @@ import { RiImageAddFill } from 'react-icons/ri'
 import { v4 as uuidv4 } from 'uuid'
 import _, { set } from 'lodash'
 import Lightbox from 'react-awesome-lightbox'
-
+import { getAllQuizForAdmin, postCreateNewQuestionForQuiz, postCreateNewAnswerForQuestion } from "../../../../services/apiService"
 
 const Questions = (props) => {
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-    ]
-    const [selectedQuiz, setSelectedQuiz] = useState({})
+    // const options = [
+    //     { value: 'chocolate', label: 'Chocolate' },
+    //     { value: 'strawberry', label: 'Strawberry' },
+    //     { value: 'vanilla', label: 'Vanilla' }
+    // ]
+
 
     const [questions, setQuestions] = useState(
         [
@@ -41,6 +41,26 @@ const Questions = (props) => {
         title: '',
         url: ''
     })
+
+    const [listQuiz, setListQuiz] = useState([])
+    const [selectedQuiz, setSelectedQuiz] = useState({})
+
+    useEffect(() => {
+        fetchQuiz()
+    }, [])
+
+    const fetchQuiz = async () => {
+        let res = await getAllQuizForAdmin()
+        if (res && res.EC === 0) {
+            let newQuiz = res.DT.map(item => {
+                return {
+                    value: item.id,
+                    label: `${item.id} - ${item.description}`
+                }
+            })
+            setListQuiz(newQuiz)
+        }
+    }
 
     const handleAddRemoveQuesion = (type, id) => {
         if (type === 'ADD') {
@@ -138,8 +158,22 @@ const Questions = (props) => {
         }
     }
 
-    const handleSubmitQuestionForQuiz = () => {
-        console.log(questions)
+    const handleSubmitQuestionForQuiz = async () => {
+        await Promise.all(questions.map(async (question) => {
+            const q = await postCreateNewQuestionForQuiz(
+                +selectedQuiz.value,
+                question.description,
+                question.imageFile
+            )
+            await Promise.all(question.answers.map(async (answer) => {
+                await postCreateNewAnswerForQuestion(
+                    answer.description,
+                    answer.isCorrect,
+                    q.DT.id
+                )
+            }))
+
+        }))
 
     }
 
@@ -168,7 +202,7 @@ const Questions = (props) => {
                     <Select
                         defaultValue={selectedQuiz}
                         onChange={setSelectedQuiz}
-                        options={options}
+                        options={listQuiz}
                     />
                 </div>
                 <div className="mt-3 mb-2">
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -92,12 +92,24 @@ const putUpdateQuizForAdmin = async (id, name, description, difficulty, image) =
     return axios.put('api/v1/quiz', data);
 };
 
+const postCreateNewQuestionForQuiz = (quiz_id, description, image) => {
+    const data = new FormData()
+    data.append('quiz_id', quiz_id)
+    data.append('description', description)
+    data.append('questionImage', image)
+    return axios.post('api/v1/question', data)
+}
+
+const postCreateNewAnswerForQuestion = (description, correct_answer, question_id) => {
+    return axios.post('api/v1/answer', { description, correct_answer, question_id })
+}
 
 export {
     postCreateNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin,
     postRegister, getQuizByUser, getDataQuiz,
     postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin,
-    deleteQuizForAdmin, putUpdateQuizForAdmin
+    deleteQuizForAdmin, putUpdateQuizForAdmin,
+    postCreateNewQuestionForQuiz, postCreateNewAnswerForQuestion
 
 }
